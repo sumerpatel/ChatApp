953e3fa4312146b1a7019efcd87a39dbeaba8a74
sumerpatel <sumerpatel@rathi.com>
2018-04-13 18:00:25 +0530

    Initial commit

--- app/src/main/java/com/example/sumerpatel/chatapp/activity/Register.java
+++ app/src/main/java/com/example/sumerpatel/chatapp/activity/Register.java
@@ -30,6 +30,7 @@ public class Register extends AppCompatActivity {
     String user, pass;
     TextView login;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
